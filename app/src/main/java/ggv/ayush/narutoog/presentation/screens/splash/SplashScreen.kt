3edab48cb517c8d538244503a3f1ca1ed3cda3e2
Ayushrai2005 <shweetarai@gmail.com>
2024-04-03 17:49:24 +0530

    Animate Splash Screen and Changed the status bar color for Dark theme

--- app/src/main/java/ggv/ayush/narutoog/presentation/screens/splash/SplashScreen.kt
+++ app/src/main/java/ggv/ayush/narutoog/presentation/screens/splash/SplashScreen.kt
@@ -6,12 +6,14 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import ggv.ayush.narutoog.R
 import ggv.ayush.narutoog.ui.theme.Purple500
@@ -19,11 +21,26 @@ import ggv.ayush.narutoog.ui.theme.Purple700
 
 @Composable
 fun SplashScreen(navController: NavController) {
-    Splash()
+    val degrees = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
+    LaunchedEffect(key1 = true) {
+        degrees.animateTo(
+            targetValue = 360f,
+            animationSpec = androidx.compose.animation.core.tween(
+                durationMillis = 1000,
+                delayMillis = 200
+            )
+        )
+
+    }
+    Splash(degrees.value)
+
 }
 
 @Composable
-fun Splash() {
+fun Splash(degrees : Float) {
+
     if(isSystemInDarkTheme()){
         Box(modifier = Modifier
             .background(Color.Black)
@@ -31,6 +48,7 @@ fun Splash() {
             contentAlignment = Alignment.Center
         ){
             Image(
+                modifier = Modifier.rotate(degrees),
                 painter = painterResource(id = R.drawable.logo),
                 contentDescription = null
             )
@@ -38,11 +56,18 @@ fun Splash() {
         }
     }else{
         Box(modifier = Modifier
-            .background(Brush.verticalGradient(listOf(Purple700, Purple500), startY = 0f, endY = 1000f))
+            .background(
+                Brush.verticalGradient(
+                    listOf(Purple700, Purple500),
+                    startY = 0f,
+                    endY = 1000f
+                )
+            )
             .fillMaxSize(),
             contentAlignment = Alignment.Center
         ){
             Image(
+                modifier = Modifier.rotate(degrees),
                 painter = painterResource(id = R.drawable.logo),
                 contentDescription = null
             )
@@ -51,10 +76,4 @@ fun Splash() {
     }
 
 
-}
-
-@Preview
-@Composable
-fun SplashScreenPreview() {
-    Splash()
 }
--- app/src/main/java/ggv/ayush/narutoog/ui/theme/Theme.kt
+++ app/src/main/java/ggv/ayush/narutoog/ui/theme/Theme.kt
@@ -4,29 +4,32 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(0xFF1A1A1A),
+    secondary = Color(0xFF121212),
+        tertiary = Color(0xFF1A1A1A)
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
 
-    /* Other default colors to override
+    // Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
@@ -34,7 +37,7 @@ private val LightColorScheme = lightColorScheme(
     onTertiary = Color.White,
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
-    */
+
 )
 
 @Composable
@@ -44,6 +47,7 @@ fun NarutoOGTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
+
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
@@ -55,10 +59,18 @@ fun NarutoOGTheme(
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
+
         SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            if(darkTheme){
+                val window = (view.context as Activity).window
+                window.statusBarColor = Color.Black.toArgb()
+                WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            }else{
+                val window = (view.context as Activity).window
+                window.statusBarColor = colorScheme.primary.toArgb()
+                WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = LightColorScheme.primary == Color.White
+            }
+
         }
     }
 
@@ -67,4 +79,5 @@ fun NarutoOGTheme(
         typography = Typography,
         content = content
     )
+
 }
